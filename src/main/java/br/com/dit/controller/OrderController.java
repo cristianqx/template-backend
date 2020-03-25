ad5991ac9bd56d5061e4d46226f37bf6e2921dfc
Andre Dornelas <andredornelas.araujo@gmail.com>
2020-03-25 15:42:20 -0300

    Configuracao yaml

--- src/main/java/br/com/dit/controller/OrderController.java
+++ src/main/java/br/com/dit/controller/OrderController.java
@@ -1,5 +1,21 @@
 package br.com.dit.controller;
 
-public class OrderController{
+import javax.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.dornelasit.api.model.Pedido;
+import br.com.dornelasitapi.controller.OrderApi;
+
+@RestController
+public class OrderController implements OrderApi {
+
+	@Override
+	public ResponseEntity<Void> createOrder(@Valid Pedido pedidoRessource) {
+		
+		System.out.println("Processando o pedido:" + pedidoRessource.getCodigoPedido());
+		
+		return ResponseEntity.ok().build();
+	}
 }
